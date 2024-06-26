2a40f0ac05ff4d2b37d4e379ea287e9d9041a71e
FabioBritto <fabio.tritono@gmail.com>
2024-06-26 18:33:06 -0300

    'Primeiro commit do projeto'

---
+++ Cinemark/src/lanchonete/salgados/Bebidas.java
@@ -0,0 +1,12 @@
+package lanchonete.salgados;
+
+public class Bebidas {
+
+	private static String nomeBebida;
+	private static double precoBebida;
+	
+	public Bebidas(String nomeBebida, double precoBebida) {
+		this.nomeBebida = nomeBebida;
+		this.precoBebida = precoBebida;
+	}
+}
